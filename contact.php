395b15226b79f953007b39050d5768541307ba40
Roydags <rddaguplo.cbsd@gmail.com>
2024-12-12 08:44:48 +0800

    first commit

---
+++ contact.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+</head>
+<body>
+<link rel="stylesheet" href="navhead.css">
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Audiowide&family=Edu+AU+VIC+WA+NT+Pre:wght@400..700&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
+<header>            
+            <a href="index.php"><img src="suncool-logo.jpg" alt="suncool" id="logo"></a>
+            <p id="main-title">Sun Cool Internet Shop and Computer services</p>
+<nav>
+<a href="index.php">Home</a> |
+<a href="about.php">About</a> |
+<a href="contact.php">Contact</a>
+</nav>
+  </header>
+
+  <address>
+Email us at: <a href="mailto:webmaster@example.com">rnbcafe37@gmail.com</a><br><br> 
+Contact us at: <a href="tel:09661934305">0966-1934-305</a><br><br>
+Purok Tambis, Sangkol,<br>
+Dipolog City, Zamboanga del Norte,<br>
+Philippines, 7100
+</address>
+
+<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d937.7530084015734!2d123.3475285652264!3d8.47308609156772!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x32549189d34cecdf%3A0xe383d904bb3fc748!2sRNB%20Cafe%20%26%20Computer%20Services!5e1!3m2!1sen!2sph!4v1733887943395!5m2!1sen!2sph" id="map-add" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+</body>
+</html>
---
+++ home.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+<body>
+<link rel="stylesheet" href="navhead.css">
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Audiowide&family=Edu+AU+VIC+WA+NT+Pre:wght@400..700&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
+<header>
+    <p id="main-title">Sun Cool Internet Shop & Computer Services</p>
+
+<nav>
+<a href="home.php">Home</a> |
+<a href="about.php">About</a> |
+<a href="contact.php">Contact</a>
+</nav>
+
+  </header><br><br>
+
+<main>
+
+  <h1>Your One Stop Internet and Computer Shop!!!</h1><br><br>
+
+  <section>
+  <h2>Most Popular Internet Shop and Computer Services</h2>
+  <p>Sun Cool Internet Shop and Computer Services, one of the leading internet shop in the Sangkol, Dipolog City Philippines, 
+    offers a wide range of servicess from laptop/desktop repair to internet browsing, printint, music/video editing and more. 
+    With features like free check-up, exclusive vouchers, and a costumer-friendly attendant, 
+    Suncool Internet Shop and Computer Services provides a convenient and efficient internet browsing and computer troubleshooting experience. 
+    Their responsive customer service and reliable delivery options make it a top choice for clients.</p><br>
+</section>
+
+</main>
+
+<footer>
+  <p>Owner: Roydags & Fam<br>
+  <a href="mailto:rnbcafe37@gmail.com">rnbcafe37@gmail.com</a></p>
+</footer>
+
+</body>
+</html>
